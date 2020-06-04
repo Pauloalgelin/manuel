a9c3d2298af4a2e22650f42014f23203d1c82809
RenanSchwyz <renancostalaiz@gmail.com>
2020-06-04 17:13:32 -0300

    autenticação só para envio

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -1,5 +1,5 @@
 class ManualsController < ApplicationController
-	before_action :authenticate_user!, except: [:index, :about, :search, :result]
+	before_action :authenticate_user!, only: [:new]
 
 	def index
 		@manuals = Manual.all
