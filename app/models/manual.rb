fb6449ca6e6b0a78697c0d98c4407e3338a85094
RenanSchwyz <renancostalaiz@gmail.com>
2020-06-28 21:17:09 -0300

    form de busca funcionando para palavras não exatas

--- app/models/manual.rb
+++ app/models/manual.rb
@@ -2,14 +2,18 @@ class Manual < ApplicationRecord
 	has_one_attached :path
 
 	def self.search(search)
+		variable = search
 		if search
-			Manual.where(tipo: search)
-			Manual.where(marca: search)
-			Manual.where(codigo: search)
-			Manual.where(modelo: search)
-			Manual.where(path: search)
+
+			Manual.where('tipo LIKE :search OR marca LIKE :search OR codigo LIKE :search OR modelo LIKE :search', search: "%#{search}%")
+			# Manual.where(tipo: search)
+			# Manual.where(marca: search)
+			# Manual.where(codigo: search)
+			# Manual.where(modelo: search)
+			# Manual.where(path: search)
 		else
-			Manual.all
+			return nil
+		# 	Manual.all
 		end
 	end
 end
